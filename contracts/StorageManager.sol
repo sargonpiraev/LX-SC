a31e07809944e32f68a63d799c2c70ceff1a8c2c
polycarpik <paladyina@gmail.com>
2017-03-29 12:17:37 +0300

    Fixed style

--- contracts/StorageManager.sol
+++ contracts/StorageManager.sol
@@ -1,14 +1,12 @@
-pragma solidity ^0.4.8;
+pragma solidity 0.4.8;
 
 import './Owned.sol';
 import './EventsHistoryUser.sol';
 
-
 contract StorageManager is EventsHistoryUser, Owned {
-
-	mapping (address => mapping(bytes32 => bool)) internal approvedContracts;
-	event AccessGiven(address actor, bytes32 role, uint version);
-	event AccessBlocked(address actor, bytes32 role, uint version);
+    mapping(address => mapping(bytes32 => bool)) internal approvedContracts;
+    event AccessGiven(address actor, bytes32 role, uint version);
+    event AccessBlocked(address actor, bytes32 role, uint version);
 
     function setupEventsHistory(address _eventsHistory) onlyContractOwner() returns(bool) {
         if (getEventsHistory() != 0x0) {
@@ -18,21 +16,21 @@ contract StorageManager is EventsHistoryUser, Owned {
         return true;
     }
 
-	function giveAccess(address _actor, bytes32 _role) onlyContractOwner() returns(bool) {
-		approvedContracts[_actor][_role] = true;
-		_emitAccessGiven(_actor, _role);
-		return true;
-	}
+    function giveAccess(address _actor, bytes32 _role) onlyContractOwner() returns(bool) {
+        approvedContracts[_actor][_role] = true;
+        _emitAccessGiven(_actor, _role);
+        return true;
+    }
 
-	function blockAccess(address _actor, bytes32 _role) onlyContractOwner() returns(bool) {
-		approvedContracts[_actor][_role] = false;
-		_emitAccessBlocked(_actor, _role);
-		return true;
-	}
+    function blockAccess(address _actor, bytes32 _role) onlyContractOwner() returns(bool) {
+        approvedContracts[_actor][_role] = false;
+        _emitAccessBlocked(_actor, _role);
+        return true;
+    }
 
-	function isAllowed(address _actor, bytes32 _role) constant returns(bool) {
-		return approvedContracts[_actor][_role];
-	}
+    function isAllowed(address _actor, bytes32 _role) constant returns(bool) {
+        return approvedContracts[_actor][_role];
+    }
 
     function _emitAccessGiven(address _user, bytes32 _role) internal {
         StorageManager(getEventsHistory()).emitAccessGiven(_user, _role);
@@ -49,6 +47,4 @@ contract StorageManager is EventsHistoryUser, Owned {
     function emitAccessBlocked(address _user, bytes32 _role) {
         AccessBlocked(_user, _role, _getVersion());
     }
-
 }
-
