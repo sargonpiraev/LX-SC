7c69a2ab308010623965a809152b785a5ed36340
Oleksii Matiiasevych <lastperson@gmail.com>
2017-04-05 14:11:13 +0300

    [LABX-22] Add PaymentGateway tests

--- contracts/helpers/Listener.sol
+++ contracts/helpers/Listener.sol
@@ -4,20 +4,39 @@ contract Listener {
     struct Request {
         address from;
         uint value;
-        bytes data;
+        bytes32 data;
     }
     Request[] requests;
+    mapping(bytes32 => uint) public deniesExpected;
 
     function () payable {
-        requests.push(Request(msg.sender, msg.value, msg.data));
+        bytes32 operation = sha3(msg.data);
+        requests.push(Request(msg.sender, msg.value, operation));
+        bool result = true;
+        if (deniesExpected[operation] > 0) {
+            result = false;
+            deniesExpected[operation]--;
+        }
+        assembly {
+            mstore(0, result)
+            return(0, 32)
+        }
     }
 
-    function getFromEnd(uint _index) constant returns(address, uint, bytes) {
+    function addDeny(bytes32 _operation) {
+        deniesExpected[_operation]++;
+    }
+
+    function getFromEnd(uint _index) constant returns(address, uint, bytes32) {
         return getFromStart(requests.length - 1 - _index);
     }
 
-    function getFromStart(uint _index) constant returns(address, uint, bytes) {
+    function getFromStart(uint _index) constant returns(address, uint, bytes32) {
         Request result = requests[_index];
         return (result.from, result.value, result.data);
+    }
+
+    function getCount() constant returns(uint) {
+        return requests.length;
     }
 }
