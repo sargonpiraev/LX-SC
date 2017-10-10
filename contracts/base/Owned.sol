1f9fc43b2e391514b730d89e9262b06348aaa894
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2017-10-10 15:25:57 +0300

    Add the facility to withdraw eth/erc20tokens from contract.

--- contracts/base/Owned.sol
+++ contracts/base/Owned.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.11;
 
+import './ERC20Interface.sol';
 
 /**
  * @title Owned contract with safe ownership pass.
@@ -49,5 +50,29 @@ contract Owned {
         contractOwner = pendingContractOwner;
         delete pendingContractOwner;
         return true;
+    }
+
+    /**
+    *  Withdraw given tokens from contract to owner.
+    *  This method is only allowed for contact owner.
+    */
+    function withdrawTokens(address[] tokens) onlyContractOwner {
+        for (uint i = 0; i < tokens.length; i++) {
+            ERC20Interface token = ERC20Interface(tokens[i]);
+            uint balance = token.balanceOf(this);
+            if (balance != 0) {
+                token.transfer(msg.sender, balance);
+            }
+        }
+    }
+
+    /**
+    *  Withdraw ether from contract to owner.
+    *  This method is only allowed for contact owner.
+    */
+    function withdrawEther() onlyContractOwner {
+        if (this.balance > 0)  {
+            msg.sender.transfer(this.balance);
+        }
     }
 }
