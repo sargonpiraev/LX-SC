fff509d3a22d46f954830a6528ac6e3e38b36c7d
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-02-27 11:01:57 +0300

    Common: add result codes

--- common/errors.js
+++ common/errors.js
@@ -3,6 +3,8 @@ const errorScope = {
     erc20library: 12000,
     jobcontroller: 13000,
     roles2libraryanderc20library: 14000,
+    paymentgateway: 15000,
+    paymentprocessor: 16000,
 }
 
 const errorCodes = {
@@ -25,6 +27,12 @@ const errorCodes = {
     JOB_CONTROLLER_WORK_IS_NOT_PAUSED: errorScope.jobcontroller + 6,
 
     ROLES_2_LIBRARY_AND_ERC20_LIBRARY_ADAPTER_UNSUPPORTED_CONTRACT: errorScope.roles2libraryanderc20library + 1,
+
+    PAYMENT_GATEWAY_INSUFFICIENT_BALANCE: errorScope.paymentgateway + 1,
+    PAYMENT_GATEWAY_TRANSFER_FAILED: errorScope.paymentgateway + 2,
+    PAYMENT_GATEWAY_NO_FEE_ADDRESS_DESTINATION: errorScope.paymentgateway + 3,
+
+    PAYMENT_PROCESSOR_OPERATION_IS_NOT_APPROVED: errorScope.paymentprocessor + 1,
 }
 
 module.exports = errorCodes
