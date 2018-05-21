8314b3a7389db9d43efc0af991d7b6f84e1303c0
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2018-05-21 20:22:25 +0300

    LABX-140 Deploy contracts v 0.0.4 #comment Reorder migrations steps

--- migrations/28_deploy_erc20library.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const Roles2Library = artifacts.require('./Roles2Library.sol');
-const Storage = artifacts.require('./Storage.sol');
-
-module.exports = deployer => {
-};
--- migrations/29_init_erc20library.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const Roles2Library = artifacts.require('./Roles2Library.sol');
-const StorageManager = artifacts.require('./StorageManager.sol');
-const MultiEventsHistory = artifacts.require('./MultiEventsHistory.sol');
-
-module.exports = deployer => {
-};
