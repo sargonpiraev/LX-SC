94e2f0c9d330e4e46bb2a9ff9d1742ce065669fa
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-05-22 12:10:36 +0300

    LABX-138 Update migrations: inject JobController into BoardController

--- migrations/37_init_boardcontroller.js
+++ migrations/37_init_boardcontroller.js
@@ -3,6 +3,7 @@ const BoardController = artifacts.require('./BoardController.sol');
 const Roles2Library = artifacts.require('./Roles2Library.sol');
 const StorageManager = artifacts.require('./StorageManager.sol');
 const MultiEventsHistory = artifacts.require('./MultiEventsHistory.sol');
+const JobController = artifacts.require('JobController')
 
 module.exports = deployer => {
     deployer
@@ -12,5 +13,7 @@ module.exports = deployer => {
     .then(multiEventsHistory => multiEventsHistory.authorize(BoardController.address))
     .then(() => StorageManager.deployed())
     .then(storageManager => storageManager.giveAccess(BoardController.address, 'BoardController'))
+    .then(() => BoardController.deployed())
+    .then(boardController => boardController.setJobController(JobController.address))
     .then(() => console.log("[Migration] BoardController #initialized"))
 };
