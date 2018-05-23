d9616dd3d204cc1829c980d258b5542fa6c55062
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-05-23 14:02:47 +0300

    LABX-144 Update errors code #comment update error code for BoardController

--- common/errors.js
+++ common/errors.js
@@ -15,7 +15,8 @@ const errorCodes = {
 
     BOARD_CONTROLLER_JOB_IS_ALREADY_BINDED: errorScope.boardController + 1,
     BOARD_CONTROLLER_USER_IS_ALREADY_BINDED: errorScope.boardController + 2,
-    BOARD_CONTROLLER_BOARD_IS_CLOSED: errorScope.boardController + 3,
+    BOARD_CONTROLLER_USER_IS_NOT_BINDED: errorScope.boardController + 3,
+    BOARD_CONTROLLER_BOARD_IS_CLOSED: errorScope.boardController + 4,
 
     ERC20_LIBRARY_CONTRACT_EXISTS: errorScope.erc20library + 1,
     ERC20_LIBRARY_CONTRACT_DOES_NOT_EXIST: errorScope.erc20library + 2,
