7153848e481caef186d0b6dc0e850ec02066363a
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-05-29 17:10:00 +0300

    LABX-191 Add properties in JobController#getJobsById result #comment add to return paused, pausedAt, pausedFor properties (#35)

--- contracts/JobsDataProvider.sol
+++ contracts/JobsDataProvider.sol
@@ -144,9 +144,9 @@ contract JobsDataProvider is JobDataCore, BitOps {
         return store.get(jobsCount);
     }
 
-    uint8 constant JOBS_RESULT_OFFSET = 17;
+    uint8 constant JOBS_RESULT_OFFSET = 20;
 
-    /// @notice Gets jobs details in a archived way (too little stack size
+    /// @notice Gets jobs details in an archived way (too little stack size
     /// for such amount of return values)
     /// @return {
     ///     "_gotIds": "`uint` identifier",
@@ -158,11 +158,14 @@ contract JobsDataProvider is JobDataCore, BitOps {
     ///     "_skills": "`uint` skills mask",
     ///     "_detailsIpfs": "`bytes32` details hash",
     ///     "_state": "`uint` job's state, see JobState",
+    ///     "_paused": "`bool` paused or not, '1' - paused, '0' - running",
     ///     "_defaultPay": "`uint` job's default pay size for job seekers",
     ///     "_createdAt": "`uint` publishing (creation) timestamp",
     ///     "_acceptedAt": "`uint` an offer has been accepted timestamp",
     ///     "_pendingStartAt": "`uint` pending started timestamp",
     ///     "_startedAt": "`uint` work started timestamp",
+    ///     "_pausedAt": "`uint` work's pause timestamp",
+    ///     "_pausedFor": "`uint` work's pause duration",
     ///     "_pendingFinishAt": "`uint` pending finish timestamp",
     ///     "_finishedAt": "`uint` work finished timestamp",
     ///     "_finalizedAt": "`uint` paycheck finalized timestamp"
@@ -181,14 +184,17 @@ contract JobsDataProvider is JobDataCore, BitOps {
             _results[_idx * JOBS_RESULT_OFFSET + 6] = bytes32(store.get(jobSkills, _jobIds[_idx]));
             _results[_idx * JOBS_RESULT_OFFSET + 7] = store.get(jobDetailsIPFSHash, _jobIds[_idx]);
             _results[_idx * JOBS_RESULT_OFFSET + 8] = bytes32(store.get(jobState, _jobIds[_idx]));
-            _results[_idx * JOBS_RESULT_OFFSET + 9] = bytes32(store.get(jobDefaultPay, _jobIds[_idx]));
-            _results[_idx * JOBS_RESULT_OFFSET + 10] = bytes32(store.get(jobCreatedAt, _jobIds[_idx]));
-            _results[_idx * JOBS_RESULT_OFFSET + 11] = bytes32(store.get(jobAcceptedAt, _jobIds[_idx]));
-            _results[_idx * JOBS_RESULT_OFFSET + 12] = bytes32(store.get(jobPendingStartAt, _jobIds[_idx]));
-            _results[_idx * JOBS_RESULT_OFFSET + 13] = bytes32(store.get(jobStartTime, _jobIds[_idx]));
-            _results[_idx * JOBS_RESULT_OFFSET + 14] = bytes32(store.get(jobPendingFinishAt, _jobIds[_idx]));
-            _results[_idx * JOBS_RESULT_OFFSET + 15] = bytes32(store.get(jobFinishTime, _jobIds[_idx]));
-            _results[_idx * JOBS_RESULT_OFFSET + 16] = bytes32(store.get(jobFinalizedAt, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 9] = bytes32(store.get(jobPaused, _jobIds[_idx]) ? 1 : 0);
+            _results[_idx * JOBS_RESULT_OFFSET + 10] = bytes32(store.get(jobDefaultPay, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 11] = bytes32(store.get(jobCreatedAt, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 12] = bytes32(store.get(jobAcceptedAt, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 13] = bytes32(store.get(jobPendingStartAt, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 14] = bytes32(store.get(jobStartTime, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 15] = bytes32(store.get(jobPausedAt, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 16] = bytes32(store.get(jobPausedFor, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 17] = bytes32(store.get(jobPendingFinishAt, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 18] = bytes32(store.get(jobFinishTime, _jobIds[_idx]));
+            _results[_idx * JOBS_RESULT_OFFSET + 19] = bytes32(store.get(jobFinalizedAt, _jobIds[_idx]));
 
             if (address(_boardController) != 0x0) {
                 _results[_idx * JOBS_RESULT_OFFSET + 1] = bytes32(_boardController.getJobsBoard(_jobIds[_idx]));
